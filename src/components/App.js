e3b65caa66f54dbb381a3c84d103e2da1b6edac4
pravin86n <pravin86n@gmail.com>
2020-10-30 18:28:31 -0400

    footer updater

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./ui/Theme";
@@ -6,10 +6,13 @@ import Header from "../components/ui/Header";
 import Footer from "../components/ui/Footer";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header />
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
         <Switch>
           <Route exact path="/" component={() => <div>Home</div>} />
           <Route exact path="/services" component={() => <div>Services</div>} />
@@ -37,7 +40,7 @@ function App() {
           />
           <Route exact path="/estimate" component={() => <div>Estimate</div>} />
         </Switch>
-        <Footer />
+        <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
       </BrowserRouter>
     </ThemeProvider>
   );
--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,15 +1,106 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
+
+import footerAdornment from "../../assets/Footer Adornment.svg"
 
 const useStyles = makeStyles((theme) => ({
     footer: {
         backgroundColor: theme.palette.common.blue,
-        width: "100%"
-    }
+        width: "100%",
+        zIndex: 1302,
+        position: "relative"
+    },
+    adornment: {
+        width: "25em",
+        verticalAlign: "bottom",
+        [theme.breakpoints.down("md")]: {
+            width: "21em",
+        },
+        [theme.breakpoints.down("xs")]: {
+            width: "15em",
+        },
+    },
+    mainContainer: {
+        position: "absolute"
+    },
+    link: {
+        color: "white",
+        fontFamily: "Arial",
+        fontSize: "0.75rem",
+        fontWeight: "Bold",
+        textDecoration: "none",
+    },
+    gridItem: {
+        margin: "3em"
+    },
 }))
 
-export default function Footer() {
+export default function Footer({value, setValue, selectedIndex, setSelectedIndex }) {
     const classes = useStyles()
 
-    return <footer className={classes.footer}>Example Footer</footer>
+    return <footer className={classes.footer}>
+        <Grid container className={classes.mainContainer} justify="center">
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item component={Link} to="/" onClick={() => setValue(0)} className={classes.link}>
+                        Home
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item component={Link} to="/services"  onClick={() => {setValue(1); setSelectedIndex(0)}} className={classes.link}>
+                        Services
+                    </Grid>
+                    <Grid item component={Link} to="/customsoftware" onClick={() => {setValue(1); setSelectedIndex(1)}} className={classes.link}>
+                        Custom Software Development
+                    </Grid>
+                    <Grid item component={Link} to="/mobileapps" onClick={() => {setValue(1); setSelectedIndex(2)}} className={classes.link}>
+                        Mobile App Development
+                    </Grid>
+                    <Grid item component={Link} to="/websites" onClick={() => {setValue(1); setSelectedIndex(3)}} className={classes.link}>
+                        Website Development
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item component={Link} to="/revolution" onClick={() => setValue(2)} className={classes.link}>
+                        The Revolution
+                    </Grid>
+                    <Grid item component={Link} to="/vision" onClick={() => setValue(2)} className={classes.link}>
+                        Vision
+                    </Grid>
+                    <Grid item component={Link} to="/technology" onClick={() => setValue(2)} className={classes.link}>
+                        Technology
+                    </Grid>
+                    <Grid item component={Link} to="/process" onClick={() => setValue(2)} className={classes.link}>
+                        Process
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item component={Link} to="/about" onClick={() => setValue(3)} className={classes.link}>
+                        About Us
+                    </Grid>
+                    <Grid item component={Link} to="/history" onClick={() => setValue(3)} className={classes.link}>
+                        History
+                    </Grid>
+                    <Grid item component={Link} to="/team" onClick={() => setValue(3)} className={classes.link}>
+                        Team
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.gridItem}>
+                <Grid container direction="column" spacing={2}>
+                    <Grid item component={Link} to="/contact" onClick={() => setValue(4)} className={classes.link}>
+                        Contact us
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+        <img alt="black decorative slash" src={footerAdornment} className={classes.adornment}/></footer>
 }
